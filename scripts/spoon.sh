070362758c85ca5dd5db9d69045f9acab3c35d93
Simon Murray <simondonaldmurray@gmail.com>
2022-04-26 14:47:11 +0100

    Update spoon.sh

--- scripts/spoon.sh
+++ scripts/spoon.sh
@@ -6,7 +6,7 @@ fa=/nfs/cellgeni/STAR/human/2020A_plus_Pf_3D7/combo.fa  #change to approriate ge
 im=/nfs/cellgeni/singularity/images/souporcell.sif #souporcell singularity image 
 cv=/lustre/scratch117/cellgen/cellgeni/simon/altered_vcfs/chr_filtered_2p_1kgenomes_GRCh38.vcf #change to appropriate vcf
 
-CPU=${1?Need CPU count} #cpu number inputted from submit.sh
+cpu=${1?Need CPU count} #cpu number inputted from submit.sh
 dir=${2?Need directory} #sample ID that is also directory
 thek=${3?Need k} #donor number inputted from submit.sh
 
@@ -32,6 +32,6 @@ singularity exec -B /lustre -B /nfs \
       -f $fa                  \
       -k $thek                \
       --common_variants $cv   \
-      -t $CPU -o $outdir      \
+      -t $cpu -o $outdir      \
       --skip_remap $remap     \
       --no_umi $umi
