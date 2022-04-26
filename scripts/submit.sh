752d3865229b5c938386f3da55f39c5ce75d134c
Simon Murray <simondonaldmurray@gmail.com>
2022-04-26 14:00:23 +0100

    removed remaining test command

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -14,7 +14,7 @@ k=3 #selecting number of donors
 
 if true; then
   mkdir -p logs
-  cat $sf | head -n 1 | while read name; do  
+  cat $sf | while read name; do  
     bsub -n $cpu -Rspan[hosts=1] -e logs/eee.$name.%J.txt -o logs/ooo.$name.%J.txt -q long -M $mem -a "memlimit=True" $script $cpu $name $k  
   done
 fi
