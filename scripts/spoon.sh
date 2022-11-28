8096c889b51bd4c018cc5f0cec51cf2c25e83e94
prete <martin.prete@sanger.ac.uk>
2022-11-28 01:24:04 +0000

    Add 'known_genotypes' option

--- scripts/spoon.sh
+++ scripts/spoon.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 
 fa=  #add to approriate genome i.e. /nfs/cellgeni/STAR/human/2020A/GRCh38_v32_modified.fa
 im=/nfs/cellgeni/singularity/images/souporcell.sif #souporcell singularity image 
-cv=  #add to appropriate vcf i.e. /nfs/cellgeni/simon/altered_vcfs/chr_filtered_2p_1kgenomes_GRCh38.vcf
+vcf= #add to appropriate vcf i.e. /nfs/cellgeni/simon/altered_vcfs/chr_filtered_2p_1kgenomes_GRCh38.vcf
+
+# by default the pipeline will use common variant loci or known variant loci vcf
+known_genotypes='false' # to use known genotypes per clone in population vcf mode to 'true'
 
 cpu=${1?Need CPU count} #cpu number inputted from submit.sh
 dir=${2?Need directory} #sample ID that is also directory
@@ -23,6 +26,12 @@ umi=True
 [[ ! -e "$psb" ]] && echo "No $psb in $dir" && false
 [[ ! -e "$bcd" ]] && echo "No $bcd in $dir" && false
 
+common_or_known="--common_variants"
+if [[ "$known_genotypes" = true ]]; then
+    common_or_known="--known_genotypes"
+fi
+common_or_known="$common_or_known $vcf"
+
 echo $dir ok
 mkdir $outdir
 
@@ -32,7 +41,8 @@ mkdir $outdir
       -b $bcd                 \
       -f $fa                  \
       -k $thek                \
-      --common_variants $cv   \
+      $common_or_known        \
       -t $cpu -o $outdir      \
       --skip_remap $remap     \
       --no_umi $umi
+
